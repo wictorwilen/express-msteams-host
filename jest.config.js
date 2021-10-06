53a1815f72f4068a366870c0fdcb45e5db29d350
Wictor Wilén <wictor@wictorwilen.se>
2021-10-06 15:38:54 +0200

    ✅ Finalized unit testing

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,24 @@ module.exports = {
     transform: {
         "^.+\\.tsx?$": "ts-jest"
     },
+    preset: "ts-jest/presets/js-with-ts",
+    globals: {
+        "ts-jest": {
+            tsconfig: "<rootDir>/tsconfig.json",
+            diagnostics: {
+                ignoreCodes: []
+            }
+        }
+    },
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 
     coverageDirectory: "coverage",
     collectCoverageFrom: [
-        "src/**/*.{ts,js}",
-        "!src/**/*.d.ts"
+        "/**/*.{js,jsx,ts,tsx}",
+        "!<rootDir>/node_modules/"
+    ],
+    coverageReporters: [
+        "text", "html"
     ]
 };
